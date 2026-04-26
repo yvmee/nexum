d218e3f086f41319f5d4bca7166ee53447aedaa5
Lara <73997200+yvmee@users.noreply.github.com>
2026-04-26 22:39:21 +0200

    swapped flow to gameflow

--- src/store/useGameStore.ts
+++ src/store/useGameStore.ts
@@ -8,7 +8,7 @@ import {
   evaluateNextChunk,
   chunkHasReflection,
 } from '../storydata/storyFlow';
-import { gameFlow, testFlow } from '../storydata/storyFlowData';
+import { gameFlow } from '../storydata/storyFlowData';
 import { backgrounds } from '../storydata/assetData';
 import { scoreInput } from '../lib/scoreInput';
 import { isBgmTrack, isSfxTrack, useSoundStore } from './useSoundStore';
@@ -139,7 +139,7 @@ export const useGameStore = create<GameManagerState>()(persist((set, get) => ({
   reflectionInputScores: [],
 
   // Story flow state
-  storyFlow: testFlow,
+  storyFlow: gameFlow,
   currentChunkId: null,
   activeDialogues: [],
   activeReflectionNodes: [],
